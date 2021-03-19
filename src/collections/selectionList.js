b27004c68083224e83bc1586be708382d65b3dc0
Bruno Foggia <bruno.foggia@cbyk.com.br>
2021-03-19 15:58:16 -0300

    selectionList refactore

--- src/collections/selectionList.js
+++ src/collections/selectionList.js
@@ -1,55 +1,72 @@
+import _ from "underscore-99xp";
 import bbx from "../define";
 
 const definition = {
     model: bbx.model.extend({}),
     selectionAttribute: "_selected",
     defaultSelection: null, // ['attribute', 'value']
+    initialize() {
+        this.on("ready", () => this.setDefault());
+        _.bind(bbx.collection.prototype.initialize, this)();
+    },
     getSelected() {
         var o = this.find((m) => !!m.get(this.selectionAttribute));
-        if (!o) {
-            if (this.defaultSelection) {
-                var [a, v] = this.defaultSelection;
-                var m = this.setSelectedBy(a, v, { silent: true });
-                if (m) {
-                    return m;
-                }
-            }
-            o = this.get(1);
-        }
-
         return o;
     },
+    setDefault() {
+        if (!this.getSelected() && this.defaultSelection) {
+            var [a, v] = this.defaultSelection;
+            var m = this.setSelectedBy(a, v, { silent: true });
+            if (m) {
+                this.trigger("default-selection");
+            }
+            return m;
+        }
+    },
     matchSelected(m) {
-        return m.id === this.getSelected().id;
+        return this.getSelected() && m && m.cid === this.getSelected().cid;
+    },
+    alreadySelected(m, o) {
+        o.selected = m;
+        this.trigger("selection-repeat", o);
+        return m;
     },
     setSelected(id, o = {}) {
-        this.map((m) => m.set(this.selectionAttribute, 0));
-        const m = this.get(id);
+        o.data = {};
+        o.data["id"] = id;
 
-        this.setModelSelected(m, o);
-        return m;
+        const m = this.get(id);
+        if (this.matchSelected(m)) return this.alreadySelected(m, o);
+        this.map((m) => m.set(this.selectionAttribute, 0));
+
+        return this.setModelSelected(m, o);
     },
     setSelectedByUid(uid, o = {}) {
+        o.data = {};
+        o.data["uid"] = uid;
+        const m = this.findWhere((m) => m.get("uid") === uid);
+        if (this.matchSelected(m)) return this.alreadySelected(m, o);
         this.map((m) => m.set(this.selectionAttribute, 0));
-        const m = this.filter((m) => m.get("uid") === uid)[0];
 
-        this.setModelSelected(m, o);
-        return m;
+        return this.setModelSelected(m, o);
     },
     setSelectedBy(a, v, o = {}) {
+        o.data = {};
+        o.data[a] = v;
+        const m = this.findWhere((m) => m.get(a) + "" === v + "");
+        if (this.matchSelected(m)) return this.alreadySelected(m, o);
         this.map((m) => m.set(this.selectionAttribute, 0));
-        const m = this.filter((m) => m.get(a) + "" === v + "")[0];
 
-        this.setModelSelected(m, o);
-        return m;
+        return this.setModelSelected(m, o);
     },
     setModelSelected(m, o = {}) {
         if (!m) {
-            this.trigger("selection-notfound");
+            this.trigger("selection-notfound", o);
             return m;
         }
-        this.setSelected(m.id);
+        if (this.matchSelected(m)) return this.alreadySelected(m, o);
 
+        m.set(this.selectionAttribute, 1);
         if (!o.silent) this.trigger("selection");
         return m;
     },
