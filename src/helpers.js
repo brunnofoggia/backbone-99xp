391178b9ae1113b4bc0368ba517a5d471ea5a242
Bruno Foggia <bruno.foggia@cbyk.com.br>
2021-06-10 11:38:15 -0300

    added encode/decode helpers to model

--- src/helpers.js
+++ src/helpers.js
@@ -1,16 +1,34 @@
+import { base64encode, base64decode } from "nodejs-base64";
 
-var helpers = {model_prototype: {}};
+var helpers = { model_prototype: {} };
 
-helpers.model_prototype.titleAttribute = 'title';
-helpers.model_prototype.titleJoinText = ' - ';
+helpers.model_prototype.titleAttribute = "title";
+helpers.model_prototype.titleJoinText = " - ";
 helpers.model_prototype.getTitle = function () {
-    var fields = this.titleAttribute.split('+'), title = [];
+    var fields = this.titleAttribute.split("+"),
+        title = [];
 
     for (let field of fields) {
         title.push(this.get(field));
     }
 
     return title.join(this.titleJoinText);
-}
+};
+
+helpers.model_prototype.getEncoded = function (a) {
+    return base64encode(this.get(a));
+};
+
+helpers.model_prototype.getDecoded = function (a) {
+    return base64decode(this.get(a));
+};
+
+helpers.model_prototype.setEncoded = function (a, v) {
+    return this.set(a, base64encode(v));
+};
+
+helpers.model_prototype.setDecoded = function (a, v) {
+    return this.set(a, base64decode(v));
+};
 
 export default helpers;
